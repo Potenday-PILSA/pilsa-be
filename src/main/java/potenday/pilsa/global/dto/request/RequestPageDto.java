24441e87d357bda4f1aae4f2418d0451170b2c02
dowoon <woodw920@gmail.com>
2024-02-02 01:58:45 +0900

    feat: RequestPageDto Lombok 추가

--- src/main/java/potenday/pilsa/global/dto/request/RequestPageDto.java
+++ src/main/java/potenday/pilsa/global/dto/request/RequestPageDto.java
@@ -1,14 +1,17 @@
 package potenday.pilsa.global.dto.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.domain.Pageable;
 
 import static lombok.AccessLevel.PROTECTED;
 import static org.springframework.data.domain.PageRequest.of;
 
 @Getter
-@NoArgsConstructor(access = PROTECTED)
+@Setter
+@NoArgsConstructor
 public class RequestPageDto {
 
     private Integer page;
@@ -18,6 +21,7 @@ public class RequestPageDto {
         return of(page, size);
     }
 
+    @Builder
     public RequestPageDto(Integer page, Integer size) {
         this.page = page;
         this.size = size;
