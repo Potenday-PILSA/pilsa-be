041fc301d9abfd589592d5ced5f0d6c917e11b2a
Dowoon <woodw920@gmail.com>
2024-02-03 23:23:15 +0900

    fix: [#26] cors 오류 수정 (#39)
    * fix: cors 에러 수정
    
    * fix: cors 에러 수정

--- src/main/java/potenday/pilsa/global/config/CorsConfig.java
+++ src/main/java/potenday/pilsa/global/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://localhost:3000", "http://api.dev.textbucket.me", "http://www.api.dev.textbucket.me", "https://api.dev.textbucket.me", "https://www.api.dev.textbucket.me")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .exposedHeaders(AUTHORIZATION)
                 .allowCredentials(true);
